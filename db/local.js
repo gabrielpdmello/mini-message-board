d9bdb3b98521e99db56c10873d4b99e422dcbb13
Gabriel Mello <gabrielpdmello@gmail.com>
2026-04-25 18:19:41 -0300

    Add fallback when DB is not available

---
+++ db/local.js
@@ -0,0 +1,16 @@
+let messages = [
+    {
+        id: 1,
+        username: 'Amando',
+        message: "No database available!!",
+        added: new Date()
+    },
+    {
+        id: 2,
+        username: 'Charles',
+        message: "Saving data locally!",
+        added: new Date()
+    },
+]
+
+module.exports = messages
--- db/queries.js
+++ db/queries.js
@@ -1,16 +1,45 @@
 const pool = require("./pool");
+const local = require("./local")
 
 async function getAllMessages() {
-  const { rows } = await pool.query("SELECT * FROM messages");
+  let rows;
+  try {
+    result = await pool.query("SELECT * FROM messages");
+    rows = result.rows;
+  } catch {
+    rows = local;
+  }
   return rows;
 }
 
 async function insertMessage(username, message) {
-  await pool.query("INSERT INTO messages (username, message, added) VALUES ($1, $2, NOW())", [username, message]);
+  try {
+    await pool.query("INSERT INTO messages (username, message, added) VALUES ($1, $2, NOW())", [username, message]);
+  } catch {
+    local.push({
+        id: local.length + 1,
+        username: `${username}`,
+        message: `${message}`,
+        added: new Date()
+    })
+  }
 }
 
 async function findMessage(id) {
-  const { rows } = await pool.query("SELECT * FROM messages WHERE id=$1", [id])
+  let rows;
+  try {
+    result = await pool.query("SELECT * FROM messages WHERE id=$1", [id]);
+    rows = result.rows;
+  } catch {
+    rows = [local.find(item => item.id == id)]
+
+    // result from query returns an array if it finds something, else it returns undefined;
+    // fallback must do the same.
+    if (rows[0] == undefined) {
+      rows = undefined;
+    }
+  }
+
   return rows;
 }
 
